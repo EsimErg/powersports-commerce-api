c2544af6d1135ceaabcb38f90688a23fbc25902b
Yessim <esergobek@gmail.com>
2026-08-04 21:08:54 +0500

    Add Torgsoft integration configuration

--- backend/powersports-commerce-api/src/test/java/kz/powersports/commerce/PowersportsCommerceApiApplicationTests.java
+++ backend/powersports-commerce-api/src/test/java/kz/powersports/commerce/PowersportsCommerceApiApplicationTests.java
@@ -3,11 +3,14 @@ package kz.powersports.commerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"woocommerce.consumer-key=test-consumer-key",
+		"woocommerce.consumer-secret=test-consumer-secret",
+		"woocommerce.webhook-secret=test-webhook-secret"
+})
 class PowersportsCommerceApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
